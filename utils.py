96c939dc1fa9d867d37d212ee5fb83c271a4253f
BoJum <panchanok@Panchanoks-MacBook-Air.local>
2020-10-31 23:39:34 -0400

    debugged

--- utils.py
+++ utils.py
@@ -16,19 +16,33 @@ def timestamp_to_env_time(ts, delta_t=15, t0=0):
     int or array-like
         transformed environment time
     """
-    import pandas as pd
 
-    index_conversion_table = pd.read_csv('data/interval_index_table_0.csv')
-    if t=15:
-        rounded_q_time = pd.Series(pd.to_datetime(ts)).dt.round('15min').dt.time.values
-        return np.array([index_conversion_table.loc[index_conversion_table['interval'] == q]['index_15m'][0]
-                        for q in rounded_q_time]) 
-    elif t=60:
+    if (t0<0) | (t0>23):
+        raise ValueError('t must be in between 0 and 23 inclusively')
+        
+    from datetime import datetime
+    import pandas as pd
+    import numpy as np
+    ts = pd.Series(pd.to_datetime(ts))
+    
+    ## convert string to pd.datetime
+    index_conversion_table = pd.read_csv('../data/interval_index_table_0.csv')
+    index_conversion_table['interval'] = pd.to_datetime(index_conversion_table['interval']).dt.time
+    
+    if delta_t==15:
+        rounded_q_time = ts.dt.round('15min').dt.time.values
+        env_time = np.array([index_conversion_table.loc[index_conversion_table['interval'] == q]['index_15m'].values[0] \
+                         for q in rounded_q_time])
+    elif delta_t==60:
         rounded_q_time = pd.Series(pd.to_datetime(ts)).dt.round('60min').dt.time.values
-        return np.array([index_conversion_table.loc[index_conversion_table['interval'] == q]['index_60m'][0]
-                        for q in rounded_q_time])
+        env_time = np.array([index_conversion_table.loc[index_conversion_table['interval'] == q]['index_60m'].values[0] \
+                         for q in rounded_q_time])
     else:
-        raise NotImplementedError('`delta_t` other 15 mins and 60 mins is not yet implemented.')
+        raise NotImplementedError('delta_t other 15 mins and 60 mins is not yet implemented.')
+    
+    if t0 != 0:
+        env_time = np.array([i+24 if i<0 else i for i in (env_time - t0)])
+    return env_time
 
 # TODO: ty & xx
 def is_adjacent(src, dst):
