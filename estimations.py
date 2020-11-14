1af875c39d5fe98078a6106181833217e50b537d
BoJum <panchanok@Panchanoks-MacBook-Air.local>
2020-11-14 13:13:24 -0500

    more time interval in timestamp_to_env...

--- estimations.py
+++ estimations.py
@@ -240,8 +240,10 @@ class Estimator:
             transformed environment time
         """
 
-        if (t0 < 0) | (t0 > 23):
-            raise ValueError('t must be in between 0 and 23 inclusively')
+#         if (t0 < 0) | (t0 > 23):
+#             raise ValueError('t must be in between 0 and 23 inclusively')
+        if t0 != 0:
+            raise NotImplementedError('t0 not equal to 0 is not yet implemented.')
 
         ts = pd.Series(pd.to_datetime(ts))
 
@@ -249,27 +251,33 @@ class Estimator:
         index_conversion_table = self.data_index
 #         index_conversion_table['interval'] = pd.to_datetime(index_conversion_table['interval']).dt.time
 
-        if delta_t == 15:
-            rounded_q_time = ts.dt.round('15min').dt.time.values
-            env_time = np.array([index_conversion_table.loc[index_conversion_table['interval'] == q]['index_15m'].values[0]
-                                 for q in rounded_q_time])
-        elif delta_t == 60:
-            rounded_q_time = pd.Series(pd.to_datetime(
-                ts)).dt.round('60min').dt.time.values
-            env_time = np.array([index_conversion_table.loc[index_conversion_table['interval'] == q]['index_60m'].values[0]
-                                 for q in rounded_q_time])
-        else:
-            raise NotImplementedError(
-                'delta_t other 15 mins and 60 mins is not yet implemented.')
+        time_index_col = 'time_index_{}m'.format(int(delta_t))
+        round_time = '{}min'.format(int(delta_t))
+        rounded_q_time = ts.dt.round(round_time).dt.time.values
+        env_time = np.array([index_conversion_table.loc[index_conversion_table['interval'] == q][time_index_col].values[0] for q in rounded_q_time])
+        
+#         if delta_t == 15:
+#             rounded_q_time = ts.dt.round('15min').dt.time.values
+#             env_time = np.array([index_conversion_table.loc[index_conversion_table['interval'] == q]['index_15m'].values[0]
+#                                  for q in rounded_q_time])
+#         elif delta_t == 60:
+#             rounded_q_time = pd.Series(pd.to_datetime(
+#                 ts)).dt.round('60min').dt.time.values
+#             env_time = np.array([index_conversion_table.loc[index_conversion_table['interval'] == q]['index_60m'].values[0]
+#                                  for q in rounded_q_time])
+#         else:
+#             raise NotImplementedError(
+#                 'delta_t other 15 mins and 60 mins is not yet implemented.')
 
         # Handle when the time does not start at 12:00am
         if t0 != 0:
             env_time = np.array([i+24 if i < 0 else i for i in (env_time - t0)])
         
-        if len(env_time) == 1:
-            return env_time[0]
+        env_time_int = env_time.astype(int)
+        if len(env_time_int) == 1:
+            return env_time_int[0]
         else:
-            return env_time
+            return env_time_int
 
     # TODO: ty & xx
 
