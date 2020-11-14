72a0b6d26cbfc4f65c0ffd8e3b656da1a5719baa
BoJum <panchanok@Panchanoks-MacBook-Air.local>
2020-11-14 14:18:45 -0500

    delta_t to member var + update on cruise_time

--- estimations.py
+++ estimations.py
@@ -158,7 +158,7 @@ class Estimator:
         """
         # NOT VECTORIZED
         t_interval = 15
-        lookup_table = self.data_cruise15  # only implemented for 15 mins
+        lookup_table = self.data_cruise
 
         q = lookup_table.loc[(lookup_table['dropoff_datetime_index'] == t) & (lookup_table['taxizone_id'] == zone),
                              'med_cruise_time_INT']
