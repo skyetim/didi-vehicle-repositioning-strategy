6b50c8355ff5cf75d9de6a110116957ca25816aa
Xue <55117204+xxuexia@users.noreply.github.com>
2020-11-24 22:59:55 +0800

    Update bar plots of v(s) and revision on optimal action

--- utils.py
+++ utils.py
@@ -8,31 +8,30 @@ import networkx as nx
 import pickle
 
 
-def plot_optimal_q(q_path = 'Q1.pkl', shp_file = 'taxi_zones.shp', t = 32):
-    
+def plot_optimal_q(q_path='Q1.pkl', shp_file='taxi_zones.shp', t=32):
     q = open(q_path, 'rb')
     data = pickle.load(q)
-
     edge = [] # action to another zone
     wait = [] # wait in the same zone
     self = [] # action within the same zone
+    a = np.zeros((264,))
+    v = []
     for i in data:
         if i[1]==t:
             now = data[i]
             # if there is optimal action
-            if (max(now)!=0):
+            if ((sum(np.equal(now,a))!=264)):
                 start = int(i[0])
                 end = int(np.argmax(now))
+                v.append([start, np.amax(now)])
                 if (end == 0):
                     wait.append(start)
                 elif (start == end):
                     self.append(end)
                 else:
                     edge.append((start, end))
-                    
     G = nx.DiGraph()
     taxi_zones = fiona.open(shp_file)
-
     color=[] # red for action, blue for weight
     width = [] # bolder if there is optimal action
     for i in range(len(taxi_zones)):
@@ -50,19 +49,39 @@ def plot_optimal_q(q_path = 'Q1.pkl', shp_file = 'taxi_zones.shp', t = 32):
         else:
             color.append('black')
             width.append(1)
-    
     # add edge
     G.add_edges_from(edge)
-    
-    
     p = nx.get_node_attributes(G,'pos')
-    plt.figure(3,figsize=(30,30)) 
+    fig = plt.figure(3,figsize=(30,30)) 
     nx.draw_networkx_nodes(G, pos=p, node_color='white', node_size=500, edgecolors = color, linewidths=width)
     nx.draw_networkx_labels(G, pos=p, label=G.nodes, font_size=10)
     nx.draw_networkx_edges(G, pos = p, width=3, edge_color='red')
     ax = plt.gca() # get the current axis
     ax.collections[0].set_edgecolor(color) 
-    # plt.savefig('../graph2.png')
+    fig.suptitle('Optimal Action for Taxi Zone', fontsize=30, y=0.9)
+    #plt.savefig('../optimal_q.png', bbox_inches = 'tight')
     plt.show()
+    
+    return v
 
-    return
+
+
+def plot_v_s(v):
+    df = pd.DataFrame(v, columns =['s', 'max_1']) 
+    df = df.sort_values(by = 's') 
+    df['s'] = df['s'].astype(str)
+
+
+    fig = plt.figure(figsize=(18,6)) 
+    plt.bar(df['s'], df['max_1'])
+    plt.xticks(rotation=30, horizontalalignment="center")
+    ax = plt.gca()
+    myLocator = mticker.MultipleLocator(10)
+    ax.xaxis.set_major_locator(myLocator)
+    
+    plt.xlabel('Taxi Zone ID') 
+    plt.ylabel('Maximum q') 
+    plt.title('Maximum q for Taxi Zone at Given Time') 
+    #plt.savefig('../v_s.png', bbox_inches = 'tight')
+    plt.show() 
+    return 
